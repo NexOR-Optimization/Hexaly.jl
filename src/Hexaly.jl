f178960032ff2ffbcd7e99a41400dc5d05d7c209
Benoît Legat <benoit.legat@gmail.com>
2026-05-26 11:05:14 +0200

    Simplify

--- src/Hexaly.jl
+++ src/Hexaly.jl
@@ -3,6 +3,7 @@ module Hexaly
 import CondaPkg
 using PythonCall
 import MathOptInterface as MOI
+import JuMP
 
 const hexaly_optimizer = PythonCall.pynew()
 
@@ -64,6 +65,17 @@ Build a JuMP nonlinear expression representing the closed-tour cost over
 `HexalyJuMPExt` extension; calling it requires JuMP to be loaded.
 """
 function sum_distances end
+
+const op_sum_distances = JuMP.NonlinearOperator(sum_distances, :sum_distances)
+
+# Below are type piracy that can be removed once
+# https://github.com/jump-dev/JuMP.jl/pull/3451
+# is merged
+JuMP._is_real(::Array{<:Real}) = true
+
+JuMP._is_real(::Array{<:JuMP.AbstractVariableRef}) = true
+
+JuMP.moi_function(x::Array) = JuMP.moi_function.(x)
 
 include("MOI/wrapper.jl")
 include("MOI/parse.jl")
