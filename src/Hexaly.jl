8aaa482210fede7181fd5c7bf27ba052619946c1
Benoît Legat <benoit.legat@gmail.com>
2026-06-03 17:32:49 +0100

    Use MathOptVRP (#19)
    * Remove test moved to MathOptVRP
    
    * Use MathOptVRP.jl
    
    * Add MathOptVRP
    
    * Fix
    
    * Fix

--- src/Hexaly.jl
+++ src/Hexaly.jl
@@ -3,6 +3,7 @@ module Hexaly
 import CondaPkg
 using PythonCall
 import MathOptInterface as MOI
+import MathOptVRP
 import JuMP
 
 const hexaly_optimizer = PythonCall.pynew()
@@ -56,33 +57,6 @@ Create a new raw `HexalyOptimizer` Python object. This is the low-level
 Hexaly Python API. For MOI/JuMP use, prefer [`Optimizer`](@ref).
 """
 raw_optimizer() = hexaly_optimizer.HexalyOptimizer()
-
-"""
-    sum_distances(dist_matrix, nodes)
-
-Build a JuMP nonlinear expression representing the closed-tour cost over
-`nodes` using `dist_matrix` as edge weights. Defined in the
-`HexalyJuMPExt` extension; calling it requires JuMP to be loaded.
-"""
-function sum_distances end
-
-const op_sum_distances = JuMP.NonlinearOperator(sum_distances, :sum_distances)
-
-# Below are type piracy that can be removed once
-# https://github.com/jump-dev/JuMP.jl/pull/3451
-# is merged
-JuMP._is_real(::Array{<:Real}) = true
-
-JuMP._is_real(::Array{<:JuMP.AbstractJuMPScalar}) = true
-
-JuMP.moi_function(x::Array) = JuMP.moi_function.(x)
-
-function JuMP.variable_ref_type(
-    ::Type{JuMP.GenericNonlinearExpr},
-    ::AbstractArray{T},
-) where {T<:JuMP.AbstractJuMPScalar}
-    return JuMP.variable_ref_type(T)
-end
 
 include("MOI/wrapper.jl")
 include("MOI/parse.jl")
