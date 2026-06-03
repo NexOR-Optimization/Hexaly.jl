4e68ef463b44e0522f69d50fb15ebccd8f40ff96
Benoît Legat <benoit.legat@gmail.com>
2026-06-03 11:09:14 +0100

    Generate C API with Clang

---
+++ gen/prologue.jl
@@ -0,0 +1,7 @@
+# Copyright (c) 2025 Benoît Legat and contributors
+#
+# Use of this source code is governed by an MIT-style license that can be found
+# in the LICENSE.md file or at https://opensource.org/licenses/MIT.
+
+# Disable JuliaFormatter for this file.
+#!format:off
